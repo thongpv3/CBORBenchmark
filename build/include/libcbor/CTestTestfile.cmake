0d0ca018dde90fea067a462aea429f17587360b5
Pham Van Thong <thongpv87@gmail.com>
2017-04-23 14:53:10 +0700

    Complete tinycbor

--- build/include/libcbor/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thongpv3/CLionProjects/CBORBenchmark/include/libcbor
-# Build directory: /home/thongpv3/CLionProjects/CBORBenchmark/build/include/libcbor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("examples")
